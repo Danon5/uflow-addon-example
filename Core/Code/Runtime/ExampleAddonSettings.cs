cdf4771509adc7d46c3a8a9fc091ad561ea81fd5
harrypantsguy <harrypants2014@gmail.com>
2023-07-03 15:44:45 -0700

    Added default example addon settings

--- Core/Code/Runtime/ExampleAddonSettings.cs
+++ Core/Code/Runtime/ExampleAddonSettings.cs
@@ -1,7 +1,9 @@
 ﻿using UFlow.Core.Runtime;
+using UnityEngine;
 
 namespace UFlowAddons.Example.Core.Code.Runtime
 {
+    [CreateAssetMenu(menuName = MENU_NAME + nameof(ExampleAddonSettings), fileName = nameof(ExampleAddonSettings))]
     public class ExampleAddonSettings : BaseAddonSettings
     {
         public string exampleSetting;
