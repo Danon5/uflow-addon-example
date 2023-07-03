9a86634bd6705a66ada2555609cbd582e60d04d2
harrypantsguy <harrypants2014@gmail.com>
2023-07-03 15:28:51 -0700

    Added example addon settings

---
+++ Core/Code/Runtime/ExampleAddonSettings.cs
@@ -0,0 +1,9 @@
+﻿using UFlow.Core.Runtime;
+
+namespace UFlowAddons.Example.Core.Code.Runtime
+{
+    public class ExampleAddonSettings : BaseAddonSettings
+    {
+        public string exampleSetting;
+    }
+}
